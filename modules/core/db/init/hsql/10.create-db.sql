274aadaa5f5d1adc3b1d5534bdbbb588d0069fec
Gleb Gorelov <gorelov@haulmont.com>
2017-06-27 15:54:27 +0400

    Add missing db scripts

--- modules/core/db/init/hsql/10.create-db.sql
+++ modules/core/db/init/hsql/10.create-db.sql
@@ -27,8 +27,25 @@ create table SAMPLE_SERVICE (
     --
     TYPE_ID varchar(36) not null,
     AMOUNT decimal(19, 2) not null,
+    DETAILING_ID varchar(36),
     DATE_ date not null,
     --
     primary key (ID)
 )^
 -- end SAMPLE_SERVICE
+-- begin SAMPLE_DETAILING
+create table SAMPLE_DETAILING (
+    ID varchar(36) not null,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    DESCRIPTION longvarchar not null,
+    --
+    primary key (ID)
+)^
+-- end SAMPLE_DETAILING
---
+++ modules/core/db/update/hsql/17/170627-2-updateService.sql
@@ -0,0 +1,1 @@
+alter table SAMPLE_SERVICE add column DETAILING_ID varchar(36) ;
